72b7fbffcd64896609422676a681fcb48f4dace3
Jeanie Chea <chea.jeanie@gmail.com>
2022-11-05 20:12:10 -0700

    Finish data gathering

--- alternative/main.py
+++ alternative/main.py
@@ -1,144 +1,115 @@
 import requests
-import pd as pandas
+import csv
+import pandas as pd
+from datetime import datetime
 
 skills = [
-
-    {
-        "id": 0,
-        "name": "Attack" 
-    },
-    {
-        "id": 1,
-        "name": "Defense"
-    },
-    {
-        "id": 2,
-        "name": "Strength"
-    },
-    {
-        "id": 3,
-        "name": "Constitution"
-    },
-    {
-        "id": 4,
-        "name": "Ranged"
-    },
-    {
-        "id": 5,
-        "name": "Prayer"
-    },
-    {
-        "id": 6,
-        "name": "Magic"
-    },
-    {
-        "id": 7,
-        "name": "Cooking"
-    },
-    {
-        "id": 8,
-        "name": "Woodcutting"
-    },
-    {
-        "id": 9,
-        "name": "Flecthing"
-    },
-    {
-        "id": 10,
-        "name": "Fishing"
-    },
-    {
-        "id": 11,
-        "name": "Firemaking"
-    },
-    {
-        "id": 12,
-        "name": "Crafting"
-    },
-    {
-        "id": 13,
-        "name": "Smithing"
-    },
-    {
-        "id": 14,
-        "name": "Mining"
-    },
-    {
-        "id": 15,
-        "name": "Herblore"
-    },
-    {
-        "id": 16,
-        "name": "Agility"
-    },
-    {
-        "id": 17,
-        "name": "Thieving"
-    },
-    {
-        "id": 18,
-        "name": "Slayer"
-    },
-    {
-        "id": 19,
-        "name": "Farming"
-    },
-    {
-        "id": 20,
-        "name": "Runecrafting"
-    },
-    {
-        "id": 21,
-        "name": "Hunter"
-    },
-    {
-        "id": 22,
-        "name": "Construction"
-    },
-    {
-        "id": 23,
-        "name": "Summmoning"
-    },
-    {
-        "id": 24,
-        "name": "Dungeoneering"
-    },
-    {
-        "id": 25,
-        "name": "Divination"
-    },
-    {
-        "id": 26,
-        "name": "Invention"
-    },
-    {
-        "id": 27,
-        "name": "Archaeology"
-    }
+    "Attack" ,
+    "Defense",
+    "Strength",
+    "Constitution",
+    "Ranged",
+    "Prayer",
+    "Magic",
+    "Cooking",
+    "Woodcutting",
+    "Fletching",
+    "Fishing",
+    "Firemaking",
+    "Crafting",
+    "Smithing",
+    "Mining",
+    "Herblore",
+    "Agility",
+    "Thieving",
+    "Slayer",
+    "Farming",
+    "Runecrafting",
+    "Hunter",
+    "Construction",
+    "Summmoning",
+    "Dungeoneering",
+    "Divination",
+    "Invention",
+    "Archaeology"
 ]
  
 def myFunc(e):
   return e['id']
-# print(skills[0]['name'])
 
-r = requests.get('https://apps.runescape.com/runemetrics/profile/profile?user=icekrystalx')
-data = r.json()
-# print(data["skillvalues"])
-player_name = data["name"]
+def read_csv(arr):
+    with open('members_lite.csv', newline='', encoding= 'unicode_escape') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            arr.append(row['Clanmate'])
+
+def write_csv(obj):
+    date = datetime.now().strftime("%m_%d_%Y")
+    date_string = "clanmate_data_" + date + ".csv"
+    df = pd.DataFrame.from_dict(data=obj)
+    df.to_csv(date_string)
+
+def replace_with_space(arr):
+    index = 0
+    while index < len(arr):
+        arr[index] = arr[index].replace('\xa0', ' ')
+        index += 1
+
+def get_player_data(arr):
+    c = []
+    for player in arr:
+        try:
+            try:
+                print(player)
+                req = requests.get(f'https://secure.runescape.com/m=hiscore/index_lite.ws?player={player}')
+
+                hiscores = req.text.split('\n')
+                skil = hiscores[1:29]
+                s = []
+
+                for i in range(0, len(skil)):
+                    
+                    s.append(skil[i].split(','))
+
+                a = {}
+                for i in range(0, len(skil)):
+                    if s[i][2] == '-1':
+                        s[i][2] = '0'
+                    a[skills[i]] = (s[i][2])
+
+                b = {"Name": player}
+                b.update(a)
+                c.append(b)
+
+            except:
+                print(player)
+                req = requests.get(f'https://apps.runescape.com/runemetrics/profile/profile?user={player}')
+                data = req.json()
+                skill_data = data["skillvalues"]
+
+                skill_data.sort(key=myFunc)
+
+                a = {}
+
+                for i in range(0, len(skill_data)):
+                    a[skills[i]] = (skill_data[i]["xp"])
+                b = {"Name": player}
+                b.update(a)
+                c.append(b)
+
+        except:
+            print(f"Not found in hiscores & Runemetrics profile private: {player}. Skipping...")
+            pass
+
+    return c
+
+if __name__ == '__main__':
+
+    players = []
+    read_csv(players)
+    replace_with_space(players)
+    player = get_player_data(players)
+    write_csv(player)
 
 
-skill_data = data["skillvalues"]
-skill_data1 = data["skillvalues"]
 
-# for i in range(0, len(data["skillvalues"])):
-#     skill_data.append([data["skillvalues"][i]["id"], data["skillvalues"][i]["xp"]])
-
-# for skill in data:
-#     # skill_data.append([skill["id"], skill["xp"]])
-#     print(skill["skillvalues"])
-skill_data.sort(key=myFunc)
-skill_data1.sort(key=myFunc)
-
-for i in range(0, len(skill_data1)):
-    skill_data1[i]["skill"] = (skills[i]["name"])
-print((skill_data1))
